e581542ab1e5dfbb3eb04b286c2460de3f8f0d5b
John Vicent <jmgvicent@gmail.com>
2025-12-30 17:34:24 +0100

    Create project. Add header component and global styles with Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import { gsap } from "gsap";
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
+});
